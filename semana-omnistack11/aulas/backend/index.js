54b436aa8e0b4511abf40cd637d2301a966f573c
Kuraigus <kuraigus@hotmail.com>
2020-03-25 12:23:50 -0300

    Some changes in index.js code

--- semana-omnistack11/aulas/backend/index.js
+++ semana-omnistack11/aulas/backend/index.js
@@ -2,11 +2,36 @@ const express = require(`express`);
 
 const app = express();
 
-app.get(`/`, (request, response) => {
+app.use(express.json());
+
+/**
+ * Rotas / Recurso
+*/
+
+/**
+ * Metodos HTTP:
+ * 
+ * GET: Buscar/listar uma informção do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back=end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query Params: Parametros nomeados enviados na rota após "?" (filtros, paginação)
+ * Route Params: Parametros utilizados para identificar recursos
+ * Request body: Corpo da requisição, utilizado para criar ou alterar recursos
+ */
+
+app.post(`/users`, (request, response) => {
+    const body = request.body;
+    
     return response.json({
         evento: `Semana Omnistack 11.0`,
         aluno: `Joao Victor Guntijo da Silva`
-    })
+    });
 });
 
 app.listen(3333);
