ee855de1c69928f2828ef7f34301969db5272e0b
Kuraigus <kuraigus@hotmail.com>
2020-03-27 04:52:01 -0300

    Front end updated

--- semana-omnistack11/aulas/frontend/src/routes.js
+++ semana-omnistack11/aulas/frontend/src/routes.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Logon from './pages/Logon';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident'
+
 
 export default function routes() {
     return (
@@ -10,6 +13,8 @@ export default function routes() {
             <Switch>
                 <Route path="/" exact component={Logon} />
                 <Route path="/register" component={Register} />
+                <Route path="/Profile" component={Profile} />
+                <Route path="/Incidents/new" component={NewIncident} />
             </Switch>
         </BrowserRouter>
     )
